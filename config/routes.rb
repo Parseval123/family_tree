fee7b576c168498dcb7d9711778ae290bbeb4a94
Flavio Forlini <lordparseval@gmail.com>
2021-08-06 09:59:32 +0200

    orchid project homepage done admins devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  get 'static_pages/home'
+  devise_for :admins
+  root to: "static_pages#home"
+  #get 'static_pages/home'
   get 'static_pages/help'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
